ba98bbf5d1803331956da2ee4c2a487a388f917f
ConArrr <conarrr7714@gmail.com>
2020-12-22 00:53:11 +0900

    Completed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React, { FC } from 'react'
-import { Route, Routes } from 'react-router'
+import { Navigate, Route, Routes } from 'react-router'
 import Home from 'containers/pages/Home'
 
 import './App.css'
+import Members from 'containers/pages/Members'
 
 const App: FC = () => (
   <>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/:orgCode/members" /** Show Component */ />
-      <Route path="*" /** Default Rooting */ />
+      <Route path="/:orgCode/members" element={<Members />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   </>
 )
